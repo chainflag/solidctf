2498f95886b971c7eec8322f61cdb870940e877e
iczc <iczcalan@gmail.com>
2021-06-28 17:30:44 +0800

    main example

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from core.account import Account
+from core.build import Build
+from core.contract import Contract
+
+account = Account()
+print(account.address)
+
+build = Build("challenge")
+contract = Contract(build.get("Greeter"))
+print(contract.deploy.estimate_gas())
+
+print(account.get_contract_address())
+tx_hash = account.deploy(contract, "HelloWorld")
+print(tx_hash)
--- server/main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import sys
-
-print ("Enter your name:")                
-sys.stdout.flush()
-my_name = sys.stdin.readline().strip()
-print ("Your name is %s" % my_name)
-sys.stdout.flush()
-quit()
