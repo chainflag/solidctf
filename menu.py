7f68da847363d90e86c0e544640bc3d23134b0a1
iczc <iczcalan@gmail.com>
2021-07-01 17:55:33 +0800

    fix path

--- menu.py
+++ menu.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 from typing import List, Dict
@@ -23,7 +24,7 @@ You can finish this challenge in a lot of connections.
 
 class _MenuBase:
     def __init__(self) -> None:
-        self._build: Build = Build("challenge")
+        self._build: Build = Build(os.path.join(os.path.dirname(__file__), "challenge"))
         self._option: List = [None, self.create_game_account, self.deploy_contract, self.request_flag,
                               self.get_contract_source]
 
