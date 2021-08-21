470579f15c77323581068c0dabbbb85357746d16
iczc <iczcalan@gmail.com>
2021-08-21 18:37:27 +0800

    generate TOKEN_SECRET in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -z "$TOKEN_SECRET" ]; then
+  TOKEN_SECRET=$(python -c "import secrets; print(secrets.token_hex(32))")
+  export TOKEN_SECRET
+fi
+
 for f in /startup/*; do
     echo "[+] running $f"
     bash "$f"
