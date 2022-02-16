2b4f58b1252883fedb4f129f2baa94a030c30bf9
iczc <iczcalan@gmail.com>
2022-02-16 23:25:13 +0800

    patch the solc download base

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-if ! brownie compile; then
+read -r -d '' COMPILE <<- 'EOF'
+from brownie import project
+from solcx import install
+
+install.BINARY_DOWNLOAD_BASE = "https://cdn.jsdelivr.net/gh/ethereum/solc-bin@latest/{}-amd64/{}"
+project.load(".")
+EOF
+
+
+if ! python3 -c "$COMPILE"; then
   exit 1
 fi
 
