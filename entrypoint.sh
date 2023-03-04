bb6db492274af14917f3116d02636ab3cb81de8b
iczc <iczcalan@gmail.com>
2023-03-04 23:59:39 +0800

    add preload and error-logfile for gunicorn args

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,9 +15,11 @@ fi
 gunicorn server:app \
   --bind "${HTTP_HOST:-127.0.0.1}":8000 \
   --daemon \
+  --preload \
   --workers 4 \
   --worker-class uvicorn.workers.UvicornWorker \
   --access-logfile /var/log/ctf/gunicorn.access.log \
+  --error-logfile /var/log/ctf/gunicorn.error.log \
   --capture-output
 
 source /xinetd.sh
