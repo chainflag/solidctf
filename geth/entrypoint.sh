18b79c8b610d7b139a1d5649cf4edc740d43daa4
iczc <iczcalan@gmail.com>
2023-03-10 23:41:56 +0800

    rename geth pre-funded address env name

--- geth/entrypoint.sh
+++ geth/entrypoint.sh
@@ -7,23 +7,23 @@ GETH_KEYSTORE_DIR=$GETH_DATA_DIR/keystore
 
 CHAIN_ID="${CHAIN_ID:-$((RANDOM + 10000))}"
 
-[ -f "$GETH_DATA_DIR/block-signer-address" ] && BLOCK_SIGNER_ADDRESS_WITHOUT_0X=$(cat "$GETH_DATA_DIR/block-signer-address")
+[ -f "$GETH_DATA_DIR/alloc-address" ] && ALLOC_ADDRESS_WITHOUT_0X=$(cat "$GETH_DATA_DIR/alloc-address")
 
 if [ ! -d "$GETH_KEYSTORE_DIR" ]; then
     echo "$GETH_KEYSTORE_DIR missing, running account import"
-    echo -n "${BLOCK_SIGNER_PRIVATE_KEY_PASSWORD:-chainflag}" >"$GETH_DATA_DIR/password"
-    echo -n "$BLOCK_SIGNER_PRIVATE_KEY" >"$GETH_DATA_DIR/block-signer-key"
-    BLOCK_SIGNER_ADDRESS_WITHOUT_0X=$(geth account import \
+    echo -n "$ALLOC_ADDRESS_PRIVATE_KEY" >"$GETH_DATA_DIR/private-key"
+    echo -n "${ALLOC_ADDRESS_PRIVATE_KEY_PASSWORD:-chainflag}" >"$GETH_DATA_DIR/password"
+    ALLOC_ADDRESS_WITHOUT_0X=$(geth account import \
         --datadir="$GETH_DATA_DIR" \
         --password="$GETH_DATA_DIR/password" \
-        "$GETH_DATA_DIR/block-signer-key" | grep -oE '[[:xdigit:]]{40}')
-    echo -n "$BLOCK_SIGNER_ADDRESS_WITHOUT_0X" >"$GETH_DATA_DIR/block-signer-address"
+        "$GETH_DATA_DIR/private-key" | grep -oE '[[:xdigit:]]{40}')
+    echo -n "$ALLOC_ADDRESS_WITHOUT_0X" >"$GETH_DATA_DIR/alloc-address"
     echo "geth account import complete"
 fi
 
 if [ ! -d "$GETH_CHAINDATA_DIR" ]; then
     echo "$GETH_CHAINDATA_DIR missing, running init"
-    sed "s/\${CHAIN_ID}/$CHAIN_ID/g; s/\${BLOCK_SIGNER_ADDRESS_WITHOUT_0X}/$BLOCK_SIGNER_ADDRESS_WITHOUT_0X/g" /genesis.json.template >/genesis.json
+    sed "s/\${CHAIN_ID}/$CHAIN_ID/g; s/\${BLOCK_SIGNER_ADDRESS_WITHOUT_0X}/$ALLOC_ADDRESS_WITHOUT_0X/g" /genesis.json.template >/genesis.json
     geth init --datadir="$GETH_DATA_DIR" /genesis.json
     echo "geth init complete"
 fi
@@ -32,7 +32,7 @@ exec geth \
     --datadir="$GETH_DATA_DIR" \
     --password="$GETH_DATA_DIR/password" \
     --allow-insecure-unlock \
-    --unlock="$BLOCK_SIGNER_ADDRESS_WITHOUT_0X" \
+    --unlock="$ALLOC_ADDRESS_WITHOUT_0X" \
     --mine \
     --networkid="$CHAIN_ID" --nodiscover \
     --http --http.addr=0.0.0.0 --http.port=8545 \
