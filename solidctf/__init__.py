acea94f7ff85d2891f2a6cc342a88e723d7bb899
iczc <iczcalan@gmail.com>
2023-04-12 02:01:55 +0800

    bump version to 1.0.0

--- solidctf/__init__.py
+++ solidctf/__init__.py
@@ -1,3 +1,3 @@
 """SolidCTF is an infrastructure solution that simplifies the build of Solidity CTF challenges."""
-__version_info__ = (0, 9, 3)
+__version_info__ = (1, 0, 0)
 __version__ = ".".join(map(str, __version_info__))
